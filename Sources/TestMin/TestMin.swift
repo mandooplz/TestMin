7f4d107bcfaddd63a678b6b060e613f2271154a8
mandooplz <kmw4561@gmail.com>
2023-12-24 15:28:30 +0900

    Second Commit

--- Sources/TestMin/TestMin.swift
+++ Sources/TestMin/TestMin.swift
@@ -1,2 +1,86 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import Foundation
+
+//MARK: Creator
+
+
+//MARK: Interface
+
+
+//MARK: Default-Implementaion
+
+
+//MARK: Implementation
+class Province {
+    private var name : String = ""
+    private var producers : Array<Producer> = []
+    private var totalProduction : Int = 0
+    private var demand : Int = 30 // demand = 수요
+    private var price : Int = 20
+    
+    init(producers : Array<Producer>) {
+        producers.forEach { producer in
+            self.producers.append(producer)
+        }
+    }
+    
+    func getName() -> String { name }
+    func getProducers() -> some Collection<Producer> { producers }
+    func setProducers(arr : Array<Producer>) {
+        self.producers = arr
+        producers.forEach { producer in
+            self.totalProduction += producer.getProduction()
+        }
+    }
+    func getTotalProduction() -> Int { totalProduction }
+    func getDemand() -> Int { demand }
+    func getPrice() -> Int { price }
+    
+    func shortfall() -> Int
+    {
+        return getDemand() - getTotalProduction()
+
+    }
+    
+    func profit() -> Int {
+        // 매출
+        let satisfiedDemand = min(getDemand(), getTotalProduction())
+        let demandValue = satisfiedDemand * getPrice()
+        
+        
+        // 비용
+        var remainingDemand = getDemand()
+        var demandCost = 0
+        getProducers()
+            .sorted { $0.getCost() < $1.getCost() }
+            .forEach { producer in
+                let contribution = min(remainingDemand, producer.getProduction() )
+                remainingDemand -= contribution
+                demandCost += contribution * producer.getCost()
+            }
+        
+        // 이익
+        let profit = demandValue - demandCost
+        return profit
+    }
+}
+
+class Producer {
+    private weak var province : Province?
+    private var cost : Int
+    private var name : String
+    private var production : Int
+    
+    init(province: Province, cost: Int, name: String, production: Int) {
+        self.province = province
+        self.cost = cost
+        self.name = name
+        self.production = production
+    }
+    
+    func getName() -> String { name }
+    func getCost() -> Int { cost }
+    func getProduction() -> Int { production }
+    
+
+}
